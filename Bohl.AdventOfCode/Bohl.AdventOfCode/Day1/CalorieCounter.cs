172a11b69e7ee3ddc52a1e4653cbdffbb388202e
Linus Hedenberg <linus.hedenberg@norce.io>
2022-12-02 19:34:49 +0100

    Day 1: Calorie Counting

---
+++ Bohl.AdventOfCode/Bohl.AdventOfCode/Day1/CalorieCounter.cs
@@ -0,0 +1,29 @@
+﻿namespace Bohl.AdventOfCode.Day1;
+
+public static class CalorieCounter
+{
+    public static int GetTopCalories(string input)
+    {
+        var elves = input
+            .Split("\n\n")
+            .Select(elf
+                => elf
+                    .Split("\n")
+                    .Where(calorie => !string.IsNullOrWhiteSpace(calorie))
+                    .Select(int.Parse))
+            .Select(elf => elf.Sum()).OrderByDescending(elf => elf);
+        return elves.First();
+    }
+    public static int[] GetTopCalories(int top, string input)
+    {
+        var elves = input
+            .Split("\n\n")
+            .Select(elf
+                => elf
+                    .Split("\n")
+                    .Where(calorie => !string.IsNullOrWhiteSpace(calorie))
+                    .Select(int.Parse))
+            .Select(elf => elf.Sum()).OrderByDescending(elf => elf);
+        return elves.Take(top).ToArray();
+    }
+}

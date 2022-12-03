449bdd52842768a50890a214afff0a72f2dc80fd
Linus Hedenberg <linus.hedenberg@norce.io>
2022-12-03 11:02:04 +0100

    Day 3: Rucksack Reorganization part 1

---
+++ Bohl.AdventOfCode/Bohl.AdventOfCode/Day3/RucksackReorganization.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+
+namespace Bohl.AdventOfCode.Day3;
+
+public class RucksackReorganization
+{
+    public List<Rucksack> Rucksacks { get; set; }
+
+    public static RucksackReorganization ParseInputWithHalves(string input)
+    {
+        var rucksacks = input.Split('\n').Select(r => new Rucksack(r)).ToList();
+
+        return new RucksackReorganization
+        {
+            Rucksacks = rucksacks
+        };
+    }
+
+    public int MisplacedPrioritySummary()
+    {
+        return Rucksacks.Select(r => r.MisplacedItemPriority()).Sum();
+    }
+
+}
+
+public class Rucksack
+{
+    public Rucksack(string items)
+    {
+        Compartment1 = items[..(items.Length / 2)].Select(i => new Item(i)).ToArray();
+        Compartment2 = items[(items.Length / 2)..].Select(i => new Item(i)).ToArray();
+    }
+
+    public Item[] Compartment1 { get; set; }
+    public Item[] Compartment2 { get; set; }
+
+    public int MisplacedItemPriority()
+    {
+        return Compartment1.First(item1 => Compartment2.Any(item2 => item1.Priority() == item2.Priority())).Priority();
+    }
+
+    public override string ToString()
+    {
+        return $"Compartment1:\n{string.Join('\n', Compartment1.ToList().Select(i => i.ToString()))}\nCompartment2:\n{string.Join('\n', Compartment2.ToList().Select(i => i.ToString()))}";
+    }
+}
+
+public class Item
+{
+    private readonly char _character;
+
+    public Item(char character)
+    {
+        _character = character;
+    }
+
+    public int Priority()
+    {
+        int result;
+        // a-z 1-26
+        // A-Z 27-52
+        if (_character >= 'A' && _character <= 'Z')
+            result = _character - 'A' + 27;
+        else 
+            result =  _character - 'a' + 1;
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{_character}:{Priority()}";
+    }
+}
---
+++ Bohl.AdventOfCode/Bohl.AdventOfCodeTests/Day3Tests/RucksackReorganizationTests.cs
@@ -0,0 +1,28 @@
+﻿using Bohl.AdventOfCode.Day3;
+
+namespace Bohl.AdventOfCodeTests.Day3Tests;
+
+internal class RucksackReorganizationTests
+{
+    [Test]
+    public void Day3_Challenge1_Test()
+    {
+        var input = Inputs.Day3Tests.Replace("\r", "");
+
+        var elfRucksacks = RucksackReorganization.ParseInputWithHalves(input);
+        var priority = elfRucksacks.MisplacedPrioritySummary();
+
+        Assert.That(priority == 157);
+    }
+
+    [Test]
+    public void Day3_Challenge1()
+    {
+        var input = Inputs.Day3Challenge.Replace("\r", "");
+
+        var elfRucksacks = RucksackReorganization.ParseInputWithHalves(input);
+        var priority = elfRucksacks.MisplacedPrioritySummary();
+
+        Assert.That(priority == 7845);
+    }
+}

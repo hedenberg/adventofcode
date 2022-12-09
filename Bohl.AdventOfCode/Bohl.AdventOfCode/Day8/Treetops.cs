86f3b1f67008d6291355f44bd67decb4e1bc83b0
Linus Hedenberg <linus.hedenberg@norce.io>
2022-12-09 14:02:30 +0100

    Day 8: Treetop Tree House part 1

---
+++ Bohl.AdventOfCode/Bohl.AdventOfCode/Day8/Treetops.cs
@@ -0,0 +1,61 @@
+﻿using Bohl.AdventOfCode.Input;
+
+namespace Bohl.AdventOfCode.Day8;
+
+public class Treetops : IParsable<Treetops>
+{
+    public required int[][] Trees { get; set; }
+
+    public int VisibleTrees()
+    {
+        var visible = 0;
+        // All outer trees are visible
+        visible += Trees.Length * 2;
+        visible += (Trees[0].Length - 2) * 2;
+
+        for (var y = 1; y < Trees.Length - 1; y++)
+        {
+            for (var x = 1; x < Trees[y].Length - 1; x++)
+            {
+                if (IsVisible(x, y))
+                {
+                    visible++;
+                }
+            }
+        }
+
+
+        return visible;
+    }
+
+    private bool IsVisible(int x, int y)
+    {
+        var tree = Trees[y][x];
+        var leftSide = Trees[y][..x];
+        var rightSide = Trees[y][(x+1)..];
+        var upperSide = Trees[..y].Select(row => row[x]).ToArray();
+        var lowerSide = Trees[(y+1)..].Select(row => row[x]).ToArray();
+
+        var result = leftSide.All(t => t < tree) || 
+               rightSide.All(t => t < tree) || 
+               upperSide.All(t => t < tree) ||
+               lowerSide.All(t => t < tree);
+        return result;
+    }
+
+    static Treetops IParsable<Treetops>.Parse(string input, IFormatProvider? provider)
+    {
+        var treeRows = input.Rows();
+        var trees = treeRows.Select(ts => ts.ToCharArray().Select(t => int.Parse(t.ToString())).ToArray()).ToArray();
+
+        return new Treetops
+        {
+            Trees = trees,
+        };
+    }
+
+    static bool IParsable<Treetops>.TryParse(string? s, IFormatProvider? provider, out Treetops result)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Bohl.AdventOfCode/Bohl.AdventOfCodeTests/Day8Tests/TerminalTests.cs
@@ -0,0 +1,46 @@
+﻿using Bohl.AdventOfCode.Day8;
+using Bohl.AdventOfCode.Input;
+
+namespace Bohl.AdventOfCodeTests.Day8Tests;
+
+public class TreetopsTests
+{
+    [Test]
+    public void Day8_Challenge1_Test()
+    {
+        var treeTops = Inputs.Day8Tests.Parse<Treetops>();
+
+        var visibleTrees = treeTops.VisibleTrees();
+
+        Assert.That(visibleTrees == 21);
+    }
+
+    [Test]
+    public void Day8_Challenge1()
+    {
+        var treeTops = Inputs.Day8Challenge.Parse<Treetops>();
+
+        var visibleTrees = treeTops.VisibleTrees();
+
+        Assert.That(visibleTrees == 1814);
+    }
+
+    [Test]
+    public void Day8_Challenge2_Test()
+    {
+        var treeTops = Inputs.Day8Challenge.Parse<Treetops>();
+    }
+
+    //[Test]
+    //public void Day7_Challenge2()
+    //{
+    //    var totalDiskSpace = 70000000;
+    //    var requiredDiskSpace = 30000000;
+
+    //    var terminal = new Terminal(Inputs.Day7Challenge);
+
+    //    var size = terminal.SmallestDirectoryMeetingRequirementSize(totalDiskSpace, requiredDiskSpace);
+
+    //    Assert.That(size == 366028);
+    //}
+}

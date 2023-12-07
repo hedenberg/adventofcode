d0a6956493c5cef8defbfe2ad84e6a1ed7aad5a6
Linus Hedenberg <linus.hedenberg@norce.io>
2023-12-07 11:28:15 +0100

    Day 6

---
+++ aoc-2023/Bohl.AdventOfCode/Bohl.AdventOfCode.Tests/Day6.cs
@@ -0,0 +1,53 @@
+using Bohl.AdventOfCode.Day6;
+using FluentAssertions;
+
+namespace Bohl.AdventOfCode.Tests;
+
+public class Day6
+{
+    [Fact]
+    public void Day6_PartOne_Example()
+    {
+        var rows = Example1Input.Rows();
+
+        var val = BoatRace.NumberOfWins(rows);
+
+        val.Should().Be(288);
+    }
+
+    [Fact]
+    public void Day6_PartOne_Test()
+    {
+        var rows = PuzzleInput.Rows();
+
+        var val = BoatRace.NumberOfWins(rows);
+
+        val.Should().Be(1660968);
+    }
+
+    [Fact]
+    public void Day6_PartTwo_Example()
+    {
+        var rows = Example1Input.Rows();
+
+        var val = BoatRace.NumberOfWins(rows, ignoreSpaces:true);
+
+        val.Should().Be(71503);
+    }
+
+    [Fact]
+    public void Day6_PartTwo_Test()
+    {
+        var rows = PuzzleInput.Rows();
+
+        var val = BoatRace.NumberOfWins(rows, ignoreSpaces: true);
+
+        val.Should().Be(26499773);
+    }
+
+    private const string Example1Input = @"Time:      7  15   30
+Distance:  9  40  200";
+
+    private const string PuzzleInput = @"Time:        47     98     66     98
+Distance:   400   1213   1011   1540";
+}

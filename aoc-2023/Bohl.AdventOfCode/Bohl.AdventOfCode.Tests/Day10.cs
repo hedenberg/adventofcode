367de1bceac495b87b74d6302ed04b4c87736999
Linus Hedenberg <linus.hedenberg@norce.io>
2023-12-17 21:37:29 +0100

    Day 10 part 2

--- aoc-2023/Bohl.AdventOfCode/Bohl.AdventOfCode.Tests/Day10.cs
+++ aoc-2023/Bohl.AdventOfCode/Bohl.AdventOfCode.Tests/Day10.cs
@@ -32,24 +32,45 @@ public class Day10
 
         var val = PipeMaze.StepsToFarthestPoint(cells);
 
-        val.Should().Be(1842168671);
+        val.Should().Be(6806);
     }
 
     [Fact]
-    public void Day10_PartTwo_Example()
+    public void Day10_PartTwo_Example1()
     {
-        var rows = Example1Input.Rows();
+        var cells = Example3Input.Cells();
 
-        var val = 1;
+        var val = PipeMaze.StepsToFarthestPoint(cells, true);
 
-        val.Should().Be(2);
+        val.Should().Be(4);
+    }
+
+    [Fact]
+    public void Day10_PartTwo_Example2()
+    {
+        var cells = Example4Input.Cells();
+
+        var val = PipeMaze.StepsToFarthestPoint(cells, true);
+
+        val.Should().Be(8);
+    }
+
+    [Fact]
+    public void Day10_PartTwo_Example3()
+    {
+        var cells = Example5Input.Cells();
+
+        var val = PipeMaze.StepsToFarthestPoint(cells, true);
+
+        val.Should().Be(10);
     }
 
     [Fact]
     public void Day10_PartTwo_Test()
     {
-        var rows = PuzzleInput.Rows();
-        var val = 1;
+        var cells = PuzzleInput.Cells();
+
+        var val = PipeMaze.StepsToFarthestPoint(cells, true);
 
         val.Should().Be(903);
     }
@@ -65,6 +86,38 @@ L|-JF";
 SJLL7
 |F--J
 LJ.LJ";
+
+    private const string Example3Input = @"...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+
+    private const string Example4Input = @".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+
+    private const string Example5Input = @"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
 
     private const string PuzzleInput = @"L77FF.FJF|FL-7F7FF-F7F7FL.|.F-77.F--|.J7.FFLL--|7-7.7-L77F7-7.F-7F7-FF7F|77.|-JF7-FFF.L-7FFF7---|F7.|.FLF-JF-|-7---|F77FF-F-77FL77|-77.|-J.F
 L7F-|7LLFJFL-FJ7L|.FL.LJJF7.|.|L-FJFJJ7L.777L-7.F7F-|7.F-7L7-F7LF-7.|L-JJF7-J-L7|F-JJFJLJ-L||.|LL|JF7-7FJ-FJ.J.J.|FLLJ7-F-L-L|7LJ-7-JL7F7|FJ
--- aoc-2023/Bohl.AdventOfCode/Bohl.AdventOfCode/Day10/PipeMaze.cs
+++ aoc-2023/Bohl.AdventOfCode/Bohl.AdventOfCode/Day10/PipeMaze.cs
@@ -1,8 +1,10 @@
-﻿namespace Bohl.AdventOfCode.Day10;
+﻿using System.Diagnostics;
+
+namespace Bohl.AdventOfCode.Day10;
 
 internal class PipeMaze
 {
-    public static int StepsToFarthestPoint(char[][] cells)
+    public static int StepsToFarthestPoint(char[][] cells, bool enclosure = false)
     {
         var steps = 0;
 
@@ -11,11 +13,12 @@ internal class PipeMaze
         var lastLocation = start;
         var currentLocation = start;
 
+        var pipe = new List<(int, int)>();
+
         do
         {
             var symbol = cells[currentLocation.r][currentLocation.c];
 
-            //var directions = GetPossibleDirections(symbol);
             if (!TryGetPossibleDirections(symbol, out var directions))
             {
                 directions = GetStartDirection(currentLocation.r, currentLocation.c, cells);
@@ -28,14 +31,76 @@ internal class PipeMaze
             var direction = filteredDirections.First();
 
             lastLocation = currentLocation;
+            pipe.Add(lastLocation);
             currentLocation = (currentLocation.r + direction.r, currentLocation.c + direction.c);
 
             steps++;
 
-
         } while (currentLocation != start);
 
+        if (enclosure)
+        {
+            var ioCells = CopyCells(cells);
 
+            var enclosed = 0;
+            for (int r = 0; r < cells.Length; r++)
+            {
+                var row = cells[r];
+                var inside = false;
+                var onPipe = false;
+                var pipeStart = 'S';
+                for (int c = 0; c < row.Length; c++)
+                {
+                    var symbol = row[c];
+                    if (symbol == 'S')
+                        symbol = GetStartSymbol(r, c, cells);
+
+                    if (pipe.Contains((r, c)))
+                    {
+                        if (symbol is '|')
+                        {
+                            inside = !inside;
+                            onPipe = false;
+                            pipeStart = 'S';
+                            continue;
+                        }
+
+                        if (symbol is 'F' or 'L')
+                        {
+                            onPipe = true;
+                            pipeStart = symbol;
+                            continue;
+                        }
+
+                        if (pipeStart is 'L' && symbol is '7' || pipeStart is 'F' && symbol is 'J')
+                        {
+                            inside = !inside;
+                            onPipe = false;
+                            pipeStart = 'S';
+                            continue;
+                        }
+                    }
+                    else if (inside)
+                    {
+                        ioCells[r][c] = 'I';
+                        enclosed++;
+                    }
+                    else if (!inside)
+                    {
+                        ioCells[r][c] = 'O';
+                    }
+
+                    //DebugWriteLine(ioCells, r, c);
+                }
+
+                if (inside)
+                {
+
+                }
+            }
+
+            return enclosed;
+        }
         return steps / 2;
     }
 
@@ -69,6 +134,25 @@ internal class PipeMaze
         }
     }
 
+    public static char GetStartSymbol(int r, int c, char[][] map)
+    {
+        var directions = GetStartDirection(r, c, map);
+
+        if (directions.Contains((-1, 0)) && directions.Contains((1, 0)))
+            return '|';
+        if (directions.Contains((0, -1)) && directions.Contains((0, 1)))
+            return '-';
+        if (directions.Contains((-1, 0)) && directions.Contains((0, 1)))
+            return 'L';
+        if (directions.Contains((-1, 0)) && directions.Contains((0, -1)))
+            return 'J';
+        if (directions.Contains((1, 0)) && directions.Contains((0, -1)))
+            return '7';
+        if (directions.Contains((1, 0)) && directions.Contains((0, 1)))
+            return 'F';
+        return 'S';
+    }
+
     public static (int r, int c)[] GetStartDirection(int r, int c, char[][] map)
     {
         var directions = new List<(int, int)>();
@@ -96,7 +180,7 @@ internal class PipeMaze
             }
         }
 
-        if (c + 1 < map.Length)
+        if (c + 1 < map[1].Length)
         {
             if (TryGetPossibleDirections(map[r][c + 1], out var rightDirections) && rightDirections.Contains((0, -1)))
             {
@@ -122,5 +206,40 @@ internal class PipeMaze
         }
 
         return (-1, -1);
+    }
+
+    private static char[][] CopyCells(char[][] cells)
+    {
+        var ioCells = new List<char[]>();
+
+        for (int i = 0; i < cells.Length; i++)
+        {
+            var row = cells[i];
+            var target = new char[row.Length];
+            row.CopyTo(target, 0);
+            ioCells.Add(target);
+        }
+
+        return ioCells.ToArray();
+    }
+
+    private static void DebugWriteLine(char[][] cells, int? r = null, int? c = null)
+    {
+        if (r is not null && c is not null)
+        {
+            Debug.WriteLine($"Row: {r} Cell: {c}");
+        }
+        for (int i = 0; i < cells.Length; i++)
+        {
+            var row = cells[i];
+            Debug.WriteLine(new string(row));
+            //for (int j = 0; j < row.Length; j++)
+            //{
+            //    var cell = row[j];
+
+
+            //}
+        }
+        Debug.WriteLine("");
     }
 }
